472d233413475cde8a6986828ecba32aff46ef0b
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2023-09-04 21:02:22 +0300

    bonus-TS

--- src/services/carService.ts
+++ src/services/carService.ts
@@ -5,8 +5,8 @@ import {urls} from "../constants/urls";
 const carService = {
     getAll: () => apiService.get<ICar[]>(urls.cars.base),
     create: (data: ICar) => apiService.post<ICar>(urls.cars.base, data),
-    updateById: (id: number, data: ICar) => apiService.put(urls.cars.byId(id), data),
-    deleteById: (id: number) => apiService.delete(urls.cars.byId(id))
+    updateById: (id: number, data: ICar) => apiService.put<ICar>(urls.cars.byId(id), data),
+    deleteById: (id: number) => apiService.delete<void>(urls.cars.byId(id))
 }
 
 export {
